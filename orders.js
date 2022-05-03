e84b606eddd4d29b892a21174d2878fe9fcba2cf
Easton Pillay <epillay@harding.edu>
2022-05-03 16:18:22 -0500

    And for my last trick.

--- orders.js
+++ orders.js
@@ -1,6 +1,6 @@
 window.CLIPPY_CDN = "./agents/"
 clippy.load("Clippy", agent => {
-    agent.moveTo(500, 500);
+    agent.moveTo(1200, 1200);
     agent.animate()
     agent.speak("When all else fails, bind some paper together. My name is Clippy.")
     agent.animate();
