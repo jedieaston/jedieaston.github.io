a342591cc1429582d728c05086a5e8120fa438be
Easton Pillay <epillay@harding.edu>
2022-05-03 16:13:36 -0500

    I'm fine with this.

--- orders.js
+++ orders.js
@@ -1,5 +1,6 @@
 window.CLIPPY_CDN = "./agents/"
 clippy.load("Clippy", agent => {
-    agent.speak("When all else fails, bind some paper together. My name is Clippy.")
     agent.animate()
+    agent.speak("When all else fails, bind some paper together. My name is Clippy.")
+    agent.animate();
 })
